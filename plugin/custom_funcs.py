1e8eb6aed0a1facddda0c2487d20bacb8e55fd7f
lightxue <bkmgtp@gmail.com>
2013-11-01 18:00:56 +0800

    user can custom their own functions here

---
+++ plugin/custom_funcs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# author:  lightxue
+# email:   bkmgtp@gmail.com
+# version: 0.01
+# website: https://github.com/lightxue/SwissCalc
+
+# You can define your own functions here
+# Every function defined here can be called in SwissCalc
+# Your function will overwrite the built-in function if your
+#     function's name is the same with built-in function
+
+def example():
+    return 'this is an example'
