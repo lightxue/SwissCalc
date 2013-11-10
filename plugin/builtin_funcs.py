bd3b7d7e2b40b0989977fefb4f62b64b877a56c4
lightxue <bkmgtp@gmail.com>
2013-11-10 20:23:20 +0800

    add base64 codec, unicode and string escape codec, and document of the built-in functions

--- plugin/builtin_funcs.py
+++ plugin/builtin_funcs.py
@@ -8,46 +8,144 @@
 
 import math
 import hashlib
+import base64
 
 funcs = {
-    'ord' : ord,
-    'chr' : chr,
+    # String
+    'ord'  : ord,
+    'chr'  : chr,
 }
+
+# Numeric
 
 math_funcs = {var : getattr(math, var)
               for var in dir(math) if callable(getattr(math, var))}
 funcs.update(math_funcs)
 
-def lg(n):
-    return math.log(n, 2)
+def lg(x):
+    '''
+lg(x)
 
-def ln(n):
-    return math.log(n)
+Return the logarithm of x base 2.
+    '''
+    return math.log(x, 2)
+
+def ln(x):
+    '''
+lg(x)
+
+Return the logarithm of x base e.
+    '''
+    return math.log(x)
+
+# String
+
+def _print(*var):
+    '''
+print([var, ...])
+
+Print variables just like print in Python
+    '''
+    for v in var:
+        print v,
+    print
+funcs['print'] = _print
+
+def printf(fmt, *var):
+    '''
+printf(fmt, [var, ...])
+
+Print formatted string like printf in C
+    '''
+    print fmt % var
+
+def hex(x):
+    '''
+hex(x)
+
+Print hexadecimal representation of integer or string
+    '''
+    if isinstance(x ,(int, long)):
+        s = '{0:x}'.format(x)
+    elif isinstance(x, str):
+        s = x.encode('hex')
+    else:
+        raise Exception("type: %s argument can't be converted to hex" % (type(x)))
+    r = [s[i:i+2] for i in xrange(0, len(s), 2)]
+    r = [r[i:i+4] for i in xrange(0, len(r), 4)]
+    print('\n'.join(' '.join(line) for line in r))
 
 def md5(s):
+    '''
+md5(s)
+
+Return the md5 digest value of s as string of hexadecimal digits
+    '''
     m = hashlib.md5()
     m.update(s)
     return m.hexdigest()
 
 def sha1(s):
+    '''
+sha1(s)
+
+Return the sha1 digest value of s as string of hexadecimal digits
+    '''
     sha = hashlib.sha1()
     sha.update(s)
     return sha.hexdigest()
 
-def hex(x):
-    if isinstance(x ,int):
-        return __builtins__['hex'](x)
-    elif isinstance(x, str):
-        s = x.encode('hex')
-        r = [s[i:i+2] for i in xrange(0, len(s), 2)]
-        r = [r[i:i+4] for i in xrange(0, len(r), 4)]
-        print '\n'.join(' '.join(line) for line in r)
-        return 'hello'
-    else:
-        raise Exception("hex() argument can't be converted to hex")
+def b64encode(s):
+    '''
+b64encode(s)
 
-def base64(s):
-    pass
+Return the base64 encode string of s
+    '''
+    return base64.b64encode(s)
+
+def b64decode(s):
+    '''
+b64decode(s)
+
+Return the base64 decode string of s
+    '''
+    return base64.b64decode(s)
+
+def uniesc_encode(s, encoding='utf-8'):
+    '''
+uniesc_encode(s, [encoding])
+
+Convert s to unicode escape string. The default encoding of s is utf-8.
+
+Return the unicode escape string
+    '''
+    return s.decode(encoding).encode('unicode-escape')
+
+def uniesc_decode(s, encoding='utf-8'):
+    '''
+uniesc_decode(s, [encoding])
+
+Decode s from  unicode escape string to string. The default encoding of decode string is utf-8
+
+Return the decoe string
+    '''
+    return s.decode('unicode-escape').encode(encoding)
+
+def stresc_encode(s):
+    '''
+stresc_encode(s)
+
+Return the string escape value of s
+    '''
+    return s.encode('string-escape')
+
+def stresc_decode(s):
+    '''
+stresc_decode(s)
+
+Return the value of unescape string s
+    '''
+    return s.decode('string-escape')
 
 builtin_funcs = {var : globals()[var]
               for var in dir() if callable(globals()[var])}
